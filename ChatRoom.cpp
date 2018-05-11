4dc89b9a7917b48ac1e1e18aa6ddf43c1d77e8d4
Chris Beser <beserchris@Chriss-MacBook-Pro-2.local>
2018-05-11 13:21:58 -0700

    init

---
+++ ChatRoom.cpp
@@ -0,0 +1,14 @@
+#include "ChatRoom.h"
+#include <unistd.h>
+#include <getopt.h>
+#include <string>
+/*
+Options: -h --help, -o --one, -m --many, -i --init, -p --passive,
+*/
+
+
+
+int main(int argc, char** argv) {
+
+
+}
